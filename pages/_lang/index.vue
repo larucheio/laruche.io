7fcba9bd1aa83109c6f812d507f0045837a21cf2
rspt <julien@rspt.ch>
2017-04-15 13:08:04 +0200

    Remove partials

--- pages/_lang/index.vue
+++ pages/_lang/index.vue
@@ -1,13 +1,34 @@
 <template>
-  <home/>
+  <div class="Content">
+    <div class="container">
+      <h1 class="Content__Title">{{ $t('home.title') }}</h1>
+      <p>{{ $t('home.introduction') }}</p>
+    </div>
+  </div>
 </template>
 
 <script>
-import Home from '~/partials/Home.vue'
-
 export default {
-  components: {
-    Home
+  head () {
+    return {
+      title: this.$t('home.head.title'),
+      meta: [
+        { hid: 'description', name: 'description', content: this.$t('home.head.description') }
+      ]
+    }
   }
 }
 </script>
+
+<style>
+.Content
+{
+  padding: 50px 0;
+  text-align: center;
+}
+.Content__Title
+{
+  font-weight: 300;
+  padding-bottom: 30px;
+}
+</style>
